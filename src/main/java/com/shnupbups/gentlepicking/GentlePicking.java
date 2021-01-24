3f333db65fa2cbf66f83769b2eae0e66387e2f86
lilypuree <000abczyx@naver.com>
2021-01-25 03:11:54 +0900

    Ported to forge, added glms to modify pickable loot, added nether plants to pickable loot list

--- src/main/java/com/shnupbups/gentlepicking/GentlePicking.java
+++ src/main/java/com/shnupbups/gentlepicking/GentlePicking.java
@@ -1,49 +1,88 @@
 package com.shnupbups.gentlepicking;
 
-import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.player.UseBlockCallback;
-import net.fabricmc.fabric.api.tag.TagRegistry;
-
+import com.google.gson.JsonObject;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.item.BlockItem;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.loot.context.LootContext;
-import net.minecraft.loot.context.LootContextParameters;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.tag.BlockTags;
-import net.minecraft.tag.ItemTags;
-import net.minecraft.tag.Tag;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.Identifier;
+import net.minecraft.loot.LootContext;
+import net.minecraft.loot.LootParameters;
+import net.minecraft.loot.conditions.ILootCondition;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ITag;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
+import net.minecraftforge.common.loot.LootModifier;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-public class GentlePicking implements ModInitializer {
-	public static final String MOD_ID = "gentlepicking";
+import javax.annotation.Nonnull;
+import java.util.Collections;
+import java.util.List;
 
-	public static final Tag<Block> PICKABLE = TagRegistry.block(id("pickable"));
+@Mod(GentlePicking.MOD_ID)
+public class GentlePicking {
+    public static final String MOD_ID = "gentlepicking";
 
-	@Override
-	public void onInitialize() {
-		UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
-			BlockPos pos = hitResult.getBlockPos();
-			BlockState state = world.getBlockState(pos);
-			if(state.isIn(PICKABLE) && player.canModifyBlocks() && !player.shouldCancelInteraction() && world.canPlayerModifyAt(player, pos) && !player.isSpectator()) {
-				Block.dropStacks(state, world, pos);
-				world.playSound(player, pos, SoundEvents.ENTITY_ITEM_PICKUP, SoundCategory.BLOCKS, 1.0f, 1.0f);
-				world.removeBlock(pos, false);
-				return ActionResult.SUCCESS;
-			}
-			return ActionResult.PASS;
-		});
-	}
+    public static final ITag.INamedTag<Block> PICKABLE = BlockTags.makeWrapperTag("gentlepicking:pickable");
 
-	public static Identifier id(String path) {
-		return new Identifier(MOD_ID, path);
-	}
+    public GentlePicking() {
+        MinecraftForge.EVENT_BUS.addListener(this::onBlockRightClick);
+        FMLJavaModLoadingContext.get().getModEventBus().addGenericListener(GlobalLootModifierSerializer.class ,this::registerModifierSerializers);
+    }
+
+    public void registerModifierSerializers(@Nonnull final RegistryEvent.Register<GlobalLootModifierSerializer<?>> event) {
+        event.getRegistry().register(
+                new PickableLootModifer.Serializer().setRegistryName(new ResourceLocation(MOD_ID, "destroy_pickable"))
+        );
+    }
+
+    public void onBlockRightClick(PlayerInteractEvent.RightClickBlock event) {
+        BlockPos pos = event.getPos();
+        World world = event.getWorld();
+        BlockState state = world.getBlockState(pos);
+        PlayerEntity player = event.getPlayer();
+        if (state.isIn(PICKABLE) && !player.isSecondaryUseActive() && player.isAllowEdit() && world.isBlockModifiable(player, pos) && !player.isSpectator()) {
+            Block.spawnDrops(state, world, pos);
+            world.playSound(player, pos, SoundEvents.ENTITY_ITEM_PICKUP, SoundCategory.BLOCKS, 1.0f, 1.0f);
+            world.removeBlock(pos, false);
+            event.setUseBlock(Event.Result.ALLOW);
+        }
+        event.setUseBlock(Event.Result.DEFAULT);
+    }
+
+    public static class PickableLootModifer extends LootModifier {
+        protected PickableLootModifer(ILootCondition[] conditionsIn) {
+            super(conditionsIn);
+        }
+
+        @Nonnull
+        @Override
+        protected List<ItemStack> doApply(List<ItemStack> generatedLoot, LootContext context) {
+            if (context.get(LootParameters.BLOCK_STATE).isIn(PICKABLE)){
+                return Collections.emptyList();
+            }
+            return generatedLoot;
+        }
+
+        private static class Serializer extends GlobalLootModifierSerializer<PickableLootModifer> {
+            @Override
+            public PickableLootModifer read(ResourceLocation location, JsonObject object, ILootCondition[] ailootcondition) {
+                return new PickableLootModifer(ailootcondition);
+            }
+
+            @Override
+            public JsonObject write(PickableLootModifer instance) {
+                return super.makeConditions(instance.conditions);
+            }
+        }
+    }
 }
